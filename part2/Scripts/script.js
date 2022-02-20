c87b40473502cbf72b1aff69bd080f4e16025ed6
KashtakDev <60832747+EvgenyZaev@users.noreply.github.com>
2022-02-20 12:58:08 -0500

    slideshow

--- part2/Scripts/script.js
+++ part2/Scripts/script.js
@@ -7,185 +7,125 @@
  * 
  */
 
+var image_array = []; // stores 
+var image_array_length; 
+var currentImage = 0;
+var play = false;
+var intervalTimerID;
+var randomizeSlides = false;
+
 //buttons controlling slide show
 const btn_playStop = document.getElementById('btn_startStop');
 const btn_prev = document.getElementById('btn_prev');
-const btn_next = document.getElementById('btn_next');
 const btn_randomOrSequential = document.getElementById('btn_randOrSeq');
 
-var img_objects; //array storing image JSON ojects
-	
-var currentImage = 0;
-var nextImage = 1;
-var now = null;
-var play = false;
-var intervalTimerID;
-var timeoutTimerID;
-var numberOfQuestions; // stores amount of slides
-var randomizeSlides = false;
-
-//hold image objects drawn onto canvas
-var imgCurrent = document.createElement("img");
-var imgNext = document.createElement("img");
-
 //event listeners for the buttons
-btn_randomOrSequential.addEventListener('click', changeRandToSeq);
-btn_playStop.addEventListener('click', stopPlay);
-btn_prev.addEventListener('click', prevImage);
-btn_next.addEventListener('click', e => {
-	if(play){
-		stopPlay();		
-	}
-	showNextImage();
-});
-
-// when window loads read image data from file, set the button listeners
+$("#btn_next").click(showNextImage);
+$("#btn_prev").click(ShowPrevImage);
+$("#btn_startStop").click(stopPlay);
+$("#btn_randOrSeq").click(changeRandToSeq);
+// when window loads read image data by class
 window.addEventListener('load', e => {
-	loadJsonFile();
+	$(document).ready(function () {
+
+		$(".image_shown").each(function () {
+			image_array.push(this);
+		});
+		image_array[0].style.display = "inline-block";
+		image_array_length = image_array.length;
+	});
+
 });
 
-// read data from imgData.JSON file
-function loadJsonFile(){
-	var xhttpReq = new XMLHttpRequest();  
-    xhttpReq.addEventListener("load", parseAndStoreJsonResponse);
-    xhttpReq.open("GET", "imgData.JSON"); 
-    xhttpReq.send();
-}
 
-// parse json file and assign img source to first img displayed
-function parseAndStoreJsonResponse(){
-	img_objects = JSON.parse(this.responseText);
-	numberOfQuestions = img_objects.length;	
-	assignImageSources(); //assign the source for the first image displayed
-}
+//function determines which image is next and
+//depending on transition chooses displays it 
+function showNextImage() {
+	//hide current image
+	$("#" + image_array[currentImage].id).toggle();
 
-// change to/from random to Sequential
-// disable back/forward buttons on random
-function changeRandToSeq(){
-	if(randomizeSlides){
-		randomizeSlides = false;
-		btn_randomOrSequential.innerHTML = "Sequential";
-		btn_prev.disabled = false;
-		btn_next.disabled = false;
-		btn_prev.style.backgroundColor = "rgb(153, 197, 255)";
-		btn_next.style.backgroundColor = "rgb(153, 197, 255)";
+	if (randomizeSlides) {
+		selectRandom(0, image_array_length - 1);
 	} else {
-		randomizeSlides = true;
-		btn_randomOrSequential.innerHTML = "Random";
-		btn_prev.disabled = true;
-		btn_next.disabled = true;
-		btn_prev.style.backgroundColor = "grey";
-		btn_next.style.backgroundColor = "grey";
+		currentImage = incrementIndex(currentImage);
+    }
+
+	//show next iamge
+	$("#" + image_array[currentImage].id).toggle();
+}
+
+//show previuos image 
+function ShowPrevImage() {
+	$("#" + image_array[currentImage].id).toggle();
+	currentImage = decrementCircle(currentImage);
+	$("#" + image_array[currentImage].id).toggle();
+}
+
+//Function selects a random number, different from current slide
+function selectRandom(min, max) {
+	let randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+
+	while (currentImage == randomNumber) {
+		randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
 	}
+	currentImage = randomNumber;
+}
+
+// decrement number, if number was at zero set it to last index
+function decrementCircle(number) {
+	number -= 1;
+
+	if (number < 0) {
+		number = image_array_length - 1;
+	}
+
+	return number;
+}
+
+//Function increments current and next img index by one  
+function incrementIndex(number) {
+
+	if (number == image_array_length - 1) {
+		number = 0;
+	} else {
+		number++;
+	}
+	return number;
 }
 
 // if automatic transition is on function pauses it
 // if its in paused(not in play) then it turns it on
-function stopPlay(){
-	if(!play){
+function stopPlay() {
+	if (!play) {
 		play = true;
-		btn_playStop.innerHTML = "Stop";	
-		timeoutTimerID = setTimeout(showNextImage, 2000);
+		$("#btn_startStop").html("Stop");
+		intervalTimerID = setInterval(showNextImage, 2500);
 	} else {
 		play = false;
 		//reset data used in transitions
 		clearInterval(intervalTimerID);
-		clearTimeout(timeoutTimerID);
-		btn_playStop.innerHTML = "Play";
+		$("#btn_startStop").html("Play");
 	}
 }
 
-//function determines which image is next and
-//depending on transition chooses displays it 
-function showNextImage(){
-	if(randomizeSlides){
-		selectRandom(0, numberOfQuestions-1);
+// change to/from random to Sequential
+// disable back/forward buttons on random
+function changeRandToSeq() {
+	if (randomizeSlides) {
+		randomizeSlides = false;
+		$("#btn_randOrSeq").html("Sequential");
+		$("#btn_next").prop('disabled', false);
+		$("#btn_prev").prop('disabled', false);
+		$("#btn_next #btn_prev").css('background-color', "rgb(153, 197, 255)");
+		//btn_prev.style.backgroundColor = "rgb(153, 197, 255)";
+		//btn_next.style.backgroundColor = "rgb(153, 197, 255)";
 	} else {
-		incrementIndex();
+		randomizeSlides = true;
+		$("#btn_randOrSeq").html("Random");
+		$("#btn_next").prop('disabled', true);
+		$("#btn_prev").prop('disabled', true);
+		$("#btn_next #btn_prev").css('background-color', 'grey');
+		//$("#btn_prev").css('background-color', 'red');
+	
 	}
-		assignImageSources();
-		if(play)
-			timeoutTimerID = setTimeout(showNextImage, 3000);
-}
-
-//show previuos image 
-function prevImage() {
-	//stop automatically switching slides 
-	if(play)
-		stopPlay();
-
-	if(currentImage == 0){
-		currentImage = img_objects.length - 1;
-		if(img_objects.length > 1)
-			nextImage--;
-	} else {
-		nextImage = currentImage;
-		currentImage -= 1;
-	} 
-
-	assignImageSources();
-}
-
-//Function increments current and next img index by one  
-function incrementIndex(){
-	previousImage = currentImage;
-
-	if(currentImage == img_objects.length - 1){
-		currentImage = 0;
-	} else {
-		currentImage++;
-	}
-
-	if(img_objects.length > 1){
-		if(nextImage == img_objects.length - 1){
-			nextImage = 0;
-		} else {
-			nextImage++;
-		}
-	}
-}
-
-//Function selects a random number, different from current slide
-function selectRandom(min, max){
-	min = Math.ceil(min);
- 	max = Math.floor(max);
-  	let randomNumber = Math.floor(Math.random() * (max - min + 1) + min); 
-
-  	while(currentImage == randomNumber){
-  		randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
-  	} 
-  	currentImage = randomNumber;
-}
-
-//set sources for next and current images based on currentImage and 
-// nextimage indexes
-function assignImageSources(){
-	imgCurrent.src = img_objects[currentImage].imageLocation;
-	if(img_objects.length > 1)
-		imgNext.src = img_objects[nextImage].imageLocation;
-}
-
-//adds caption to context provided and xposition is used to find center of the slide
-// slide number used to draw slide caption
-function addCaption(context, xPosition, slideNumber){
-	context.fillStyle = "black";
-	context.globalAlpha = 0.4;
-	let captionHeight = CANVAS_HEIGHT * 0.2;
-	context.fillRect(0, (CANVAS_HEIGHT-captionHeight), CANVAS_WIDTH, captionHeight);
-	context.globalAlpha = 1;
-	context.font = "20px Arial";
-	context.fillStyle = "white";
-	context.textAlign = "center";
-	context.fillText(img_objects[slideNumber].imageCaption, xPosition + (CANVAS_WIDTH/2), (CANVAS_HEIGHT-captionHeight/2), (CANVAS_WIDTH*0.9));
-}
-
-// decrement number, if number was at zero set it to last index
-function decrementCircle(number){
-	number -= 1;
-
-	if(number < 0){
-		number = numberOfQuestions - 1;
-	}
-
-	return number;
 }
